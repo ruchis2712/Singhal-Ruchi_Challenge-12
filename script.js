2c7875ebbab7fb15ded9d60f2b8972af9aa5c408
Singhal <rusinghal@deloitte.com>
2019-12-23 12:54:38 -0800

    adding code and data files

---
+++ script.js
@@ -0,0 +1,38 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+})}
+  
+init();
+
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);  
+
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+
+      Object.entries(resultArray[0]).forEach(([key, value]) =>
+      {
+        PANEL.append("h6").text(key + ': ' + value);
+        });
+
+
+    });
+}
